a969c4343f091bcad57bc4dac943e02a6e5f72f5
RainaRiti <C0816968@mylambton.ca>
2021-09-20 10:27:58 -0400

    Initial Commit

---
+++ Lab_A1_A2_iOS_Ritika_C0816968/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Ritika_C0816968
+//
+//  Created by Mac on 2021-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
